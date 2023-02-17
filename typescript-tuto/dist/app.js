6422b97ebe173933b09dd7472cdd401a5d28b01c
Romain B <98778180+RomainBasly@users.noreply.github.com>
2023-02-17 12:53:31 +0100

    Delete typescript-tuto directory

--- typescript-tuto/dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const compteur = document.querySelector('#compteur');
-let index = 0;
-const increment = (e) => {
-    index++;
-    compteur.querySelector('span').innerText = index.toString();
-};
-compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
